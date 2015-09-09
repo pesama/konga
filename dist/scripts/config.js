9e1ace9dfafb862814e7b03d2573ec7fe1830f30
Pelayo Sánchez <sanchezmargareto@gmail.com>
2015-09-09 20:48:14 +0200

    more go

--- dist/scripts/config.js
+++ dist/scripts/config.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'testing',apiEndpoint:'http://api.presupuestor.com',deployPath:'/undefined',version:'vundefined',deployTime:1441822459172})
+.constant('ENV', {name:'testing',apiEndpoint:'http://api.presupuestor.com',deployPath:'/undefined',version:'vundefined',deployTime:1441824382131})
 
 ;
