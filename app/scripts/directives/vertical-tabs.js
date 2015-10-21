0189efdefde5a5a5d5835df203c4df3eef524840
Pelayo Sánchez Margareto <pelayo.sanchez@dev.bbva.com>
2015-10-21 14:31:20 +0200

    re-including vertical-tabs

---
+++ app/scripts/directives/vertical-tabs.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('ui.konga')
+  .directive('verticalTabs', function () {
+  	var directiveDefinitionObject = {};
+	directiveDefinitionObject.restrict = 'E';
+	directiveDefinitionObject.transclude= true;
+	directiveDefinitionObject.replace = true;
+	directiveDefinitionObject.scope= {};
+	
+	directiveDefinitionObject.controller= function($scope, $rootScope){
+		var tabContentList = $scope.tabContentList = [];
+		
+		$scope.select = function(tabContent){
+			angular.forEach(tabContentList, function(tabContent){
+				tabContent.selected = false;
+				tabContent.active = '';
+			});
+			tabContent.selected = true;
+			tabContent.active = 'active';
+			$rootScope.pageData.currentTab = tabContent.title;
+			$scope.$emit('changeTab', {tab: tabContent} );
+		};
+		
+		this.addTabContent = function(tabContent){
+			if(tabContentList.length === 0 && !$rootScope.pageData.currentTab){
+				$scope.select(tabContent);
+			}
+			if($rootScope.pageData.currentTab){
+				if(tabContent.title === $rootScope.pageData.currentTab){
+					$scope.select(tabContent);
+				}
+			}
+			tabContentList.push(tabContent);
+		};
+	};
+	
+	directiveDefinitionObject.templateUrl = '/views/verticaltab.tp.html';
+	
+	return directiveDefinitionObject;
+  });
