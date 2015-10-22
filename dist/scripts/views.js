575ddfc6d85643592d30a9c3aa2049bc137f64bf
Pelayo Sánchez Margareto <pelayo.sanchez@dev.bbva.com>
2015-10-22 10:56:07 +0200

    Working a bit more on update

--- dist/scripts/views.js
+++ dist/scripts/views.js
@@ -257,26 +257,28 @@ angular.module('ui.konga').run(['$templateCache', function($templateCache) {
 
   $templateCache.put('/konga/views/entity-update.html',
     "<div class=\"wall\">\n" +
-    "\t<form ng-class=\"formStyle\" role=\"form\" name=\"entityUpdate\" novalidate>\n" +
-    "\t\t<update-form entity=\"entity\" changes=\"changes\" metadata=\"entityMetadata\" params=\"params\" on-change=\"operations.changeEntityField\" on-update=\"operations.updateEntityField\" creating=\"creating\"></update-form>\n" +
-    "\t\t<div class=\"pull-right update-btn-group\" ng-if=\"showActions\">\n" +
-    "\t\t\t<button class=\"btn btn-default\" ng-repeat=\"action in entityMetadata.actions\" ng-model=\"action\" ng-click=\"operations.dispatchAction(action)\" ng-show=\"action.scope==='UPDATE'\" ng-disabled=\"action.scope!=='UPDATE'\" id=\"update-action-dispatcher-{{ action.name }}\">\n" +
-    "\t\t\t\t\t\t{{ action.label | translate }}\n" +
-    "\t\t\t</button>\n" +
-    "\t\t\t<button type=\"submit\" class=\"btn btn-success\" ng-click=\"operations.dispatchEntityAction('save', params)\" ng-disabled=\"entityUpdate.$invalid || !changes.length || invalid || (entityMetadata.name==='Materiel' && !entity.validCtrOperat && entity.id != null) || customDisableValider || alreadyValidated\" id=\"save-entity\">\t\n" +
-    "\t\t\t\t<i class=\"glyphicon glyphicon-ok\"></i>\n" +
-    "\t\t\t\t{{ 'message.action.validate' | translate }}\n" +
-    "\t\t\t</button>\n" +
-    "\t\t\t<button class=\"btn btn-default\" ng-click=\"operations.cancelUpdate()\" id=\"cancel-update\">\n" +
-    "\t\t\t\t<i class=\"glyphicon glyphicon-remove\"></i>\n" +
-    "\t\t\t\t{{ 'message.action.cancel' | translate }}\n" +
-    "\t\t\t</button>\n" +
-    "\t\t\t<button class=\"btn btn-danger\" ng-click=\"operations.deleteEntity()\" ng-hide=\"deletable == false\" id=\"delete-entity\" ng-disabled=\"disabledDelete\">\n" +
-    "\t\t\t\t<i class=\"glyphicon glyphicon-trash\"></i>\n" +
-    "\t\t\t\t{{ 'message.action.delete' | translate }}\n" +
-    "\t\t\t</button>\n" +
-    "\t\t</div>\n" +
-    "\t</form>\n" +
+    "\t<div class=\"panel-body\">\n" +
+    "\t\t<form ng-class=\"formStyle\" role=\"form\" name=\"entityUpdate\" novalidate>\n" +
+    "\t\t\t<update-form entity=\"entity\" changes=\"changes\" metadata=\"entityMetadata\" params=\"params\" on-change=\"operations.changeEntityField\" on-update=\"operations.updateEntityField\" creating=\"creating\"></update-form>\n" +
+    "\t\t\t<div class=\"pull-right update-btn-group\" ng-if=\"showActions\">\n" +
+    "\t\t\t\t<button class=\"btn btn-default\" ng-repeat=\"action in entityMetadata.actions\" ng-model=\"action\" ng-click=\"operations.dispatchAction(action)\" ng-show=\"action.scope==='UPDATE'\" ng-disabled=\"action.scope!=='UPDATE'\" id=\"update-action-dispatcher-{{ action.name }}\">\n" +
+    "\t\t\t\t\t\t\t{{ action.label | translate }}\n" +
+    "\t\t\t\t</button>\n" +
+    "\t\t\t\t<button type=\"submit\" class=\"btn btn-success\" ng-click=\"operations.dispatchEntityAction('save', params)\" ng-disabled=\"entityUpdate.$invalid || !changes.length || invalid || (entityMetadata.name==='Materiel' && !entity.validCtrOperat && entity.id != null) || customDisableValider || alreadyValidated\" id=\"save-entity\">\t\n" +
+    "\t\t\t\t\t<i class=\"glyphicon glyphicon-ok\"></i>\n" +
+    "\t\t\t\t\t{{ 'message.action.validate' | translate }}\n" +
+    "\t\t\t\t</button>\n" +
+    "\t\t\t\t<button class=\"btn btn-default\" ng-click=\"operations.cancelUpdate()\" id=\"cancel-update\">\n" +
+    "\t\t\t\t\t<i class=\"glyphicon glyphicon-remove\"></i>\n" +
+    "\t\t\t\t\t{{ 'message.action.cancel' | translate }}\n" +
+    "\t\t\t\t</button>\n" +
+    "\t\t\t\t<button class=\"btn btn-danger\" ng-click=\"operations.deleteEntity()\" ng-hide=\"deletable == false\" id=\"delete-entity\" ng-disabled=\"disabledDelete\">\n" +
+    "\t\t\t\t\t<i class=\"glyphicon glyphicon-trash\"></i>\n" +
+    "\t\t\t\t\t{{ 'message.action.delete' | translate }}\n" +
+    "\t\t\t\t</button>\n" +
+    "\t\t\t</div>\n" +
+    "\t\t</form>\n" +
+    "\t</div>\n" +
     "</div>"
   );
 
